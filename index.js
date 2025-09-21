80d1fb3db91394e4b9196bc6ee041b05e56f7d00
EfeHasircioglu <efehasircioglu.com@gmail.com>
2025-09-21 16:15:18 +0300

    complete version without finishing screen

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const targetTextSpan = document.getElementById("targetTextSpan");
 const targetInput = document.getElementById("targetInput");
 const modalContainer = document.getElementById("modalContainer");
-const modalButton = document.getElementById("modalClose");
+const modalInner = document.getElementById("resultsModal");
 const resultsText = document.getElementById("resultsText");
 const resultsWPM = document.getElementById("resultsWPM");
 const resultsElapsed = document.getElementById("resultsElapsed");
 const timeView = document.getElementById("timeView");
-//TODO: reset fonksiyonunu yapma ve tasarımı güzelleştirme
+const levelView = document.getElementById("levelView");
+const resetButton = document.getElementById("resetButton");
+//TODO: bitirince tebrik ekranı yapılabilir basit bir alert yerine
 /* levellere göre textler zorlaşacak */
 const levelTexts = [
   "Geyikler, doğal ortamda hayatta kalmaya çalışan otobur hayvanlardır.",
@@ -24,7 +26,8 @@ let selectedText = levelTexts[level];
 let selectedWords = [];
 let visibleWords = document.getElementsByClassName("inner-word");
 let xVisibleWords = Array.from(visibleWords);
-
+/* levelView'i başta 1. seviyede olacağımız için içine 1 yazıyoruz */
+levelView.innerHTML = "1";
 function renderLevel() {
   targetTextSpan.innerHTML = "";
   //bir önceki selectedWords'ü temizliyoruz
@@ -52,27 +55,54 @@ function renderLevel() {
   visibleWords = document.getElementsByClassName("inner-word");
   xVisibleWords = Array.from(visibleWords);
 }
+/* timer başlatmak için bi fonksiyon */
+function startTimer(first) {
+  /* birden fazla olmaması için önceden varsa siliyoruz. */
+  if (!first) {
+    if (timerInterval) {
+      clearInterval(timerInterval);
+    }
+  }
 
+  /* eğer yazmaya başladıysak, saniyeyi gösteren şey her saniye güncellensin */
+  timerInterval = setInterval(() => {
+    if (startTime !== null) {
+      timeView.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg> ${Math.floor(
+        (Date.now() - startTime) / 1000
+      )}s`;
+    }
+  }, 1000);
+}
+/* başta bi çalışsın, evet pek iyi değil ama bide böyle deneyelim */
+startTimer(true);
 /* React'daki useEffect mantığını vanilla JS'e uyarlama */
 function setLevel(newLevel) {
   level = newLevel;
   renderLevel();
 }
+/* modalın görünmesini sağlamak için */
+function showModal(text, wpmtext, seconds) {
+  resultsText.innerText = text;
+  resultsElapsed.innerText = seconds;
+  resultsWPM.innerText = wpmtext;
+  modalContainer.classList.remove("hidden");
+  targetInput.disabled = true;
+}
 let startTime = null;
 let wpm;
-
+let typingStarted = false;
 targetInput.addEventListener("input", () => {
   /* ilk input eventinde zamanı başlatsın  */
   if (startTime === null) {
     startTime = Date.now();
   }
+  /* yazı yazmanın başladığını belirtiyoruz */
+  typingStarted = true;
   /* bu hesaplamaları önceden yapıyoruz */
   const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
   const elapsedMinutes = elapsedTime / 60;
-  //TODO: input başlayınca timer görüntülemesi de başlasın
 
   /* her input eventinde geçen süreyi hesaplıyoruz (bu verimsiz olduğu için daha sonra değiştirilebilir) */
-
   wpm = Math.floor((correctWordCount / elapsedTime) * 60);
   let currentlyWritten = targetInput.value;
   /* ilk olarak yazarken hangisini yazdığımızı bilmemiz için yazıyor olduğumuz cümle gri olacak  */
@@ -94,21 +124,20 @@ targetInput.addEventListener("input", () => {
       targetInput.value = "";
     }
   }
-  /* modalın görünmesini sağlamak için */
-  function showModal(text, wpmtext, seconds) {
-    resultsText.innerText = text;
-    resultsElapsed.innerText = seconds;
-    resultsWPM.innerText = wpmtext;
-    modalContainer.classList.remove("hidden");
-    targetInput.disabled = true;
-  }
 
   /* eğer paragrafı yazmayı bitirmişsek doğruluk oranına göre sonraki seviyeye geçip geçmediğimizi sorguluyoruz */
   if (currentWordIndex === selectedWords.length) {
+    /* yazmamız bitince yazı yazma flagını da false yapıyoruz */
+    typingStarted = false;
+    /* intervali durduruyoruz */
+    clearInterval(timerInterval);
+    timeView.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg> None yet`;
     if (correctWordCount >= (selectedWords.length * 8) / 10 && wpm > 30) {
       if (level + 1 < levelTexts.length) {
         xVisibleWords = Array.from(visibleWords);
         setLevel(level + 1);
+        levelView.innerHTML = `${level + 1} `;
+        startTimer(false);
         showModal(
           `Congratulations! You completed the level successfully.`,
           wpm,
@@ -123,13 +152,35 @@ targetInput.addEventListener("input", () => {
       setLevel(level);
     }
   }
-  console.log(wpm);
-  console.log(elapsedMinutes);
 });
-function closeModal() {
-  modalContainer.classList.add("hidden");
-  targetInput.disabled = false;
+
+/* bu fonksiyon şu anda yazmakta olduğumuz seviyeyi resetleyecek sadece, seviyeye baştan başlayacak */
+function reset() {
+  // diğer değerleri sıfırlıyoruz
+  currentWordIndex = 0;
+  correctWordCount = 0;
+  wrongWordCount = 0;
+  startTime = null;
+  wpm = 0;
+  typingStarted = false;
+  targetInput.value = "";
+  clearInterval(timerInterval);
+  timeView.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg> None yet`;
+  xVisibleWords.forEach((word) => {
+    word.classList.remove("written");
+    word.classList.remove("correct");
+    word.classList.remove("false");
+  });
+  startTimer(false);
 }
-modalButton.addEventListener("click", () => closeModal());
+resetButton.addEventListener("click", () => reset());
+modalContainer.addEventListener("click", (e) => closeModal(e));
+
+function closeModal(e) {
+  if (e.target === modalContainer) {
+    modalContainer.classList.add("hidden");
+    targetInput.disabled = false;
+  }
+}
 
 renderLevel();
